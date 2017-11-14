08ae80b442339821bdb783967d6b7b9b0c1a55c4
Michael Leadley <leadley75@gmail.com>
2017-11-14 17:38:28 +0000

    Add tick, startPlaying, stopPlaying to App and temporarily play kick on every tick

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,40 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { togglePad, togglePlaying } from '../actions'
+import { playAudioBuffer } from '../helpers/webAudio'
 import './App.css'
 import Header from '../components/Header'
 import PlayButton from '../components/PlayButton'
 import SequencerGrid from '../components/SequencerGrid'
 
 class App extends Component {
+  constructor (props) {
+    super(props)
+    this.tick = this.tick.bind(this)
+    this.startPlaying = this.startPlaying.bind(this)
+    this.stopPlaying = this.stopPlaying.bind(this)
+  }
+
+  tick () {
+    playAudioBuffer(this.props.samples.kick)
+  }
+
+  startPlaying () {
+    this.interval = setInterval(
+      () => this.tick(),
+      60000 / 100
+    )
+  }
+
+  stopPlaying () {
+    clearInterval(this.interval)
+  }
+
+  componentWillReceiveProps (nextProps) {
+    if (nextProps.playing === this.props.playing) return
+    nextProps.playing ? this.startPlaying() : this.stopPlaying()
+  }
+
   render () {
     return (
       <div className='App'>
@@ -25,6 +53,7 @@ class App extends Component {
 }
 
 const mapStateToProps = (state, ownProps) => ({
+  samples: state.samples,
   pads: state.pads,
   playing: state.playing
 })
