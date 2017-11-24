165ca262e27848c69bfcc23e075ecdb809afaf89
Michael Leadley <leadley75@gmail.com>
2017-11-24 14:24:17 +0000

    Apply 0.6 gain to samples before output

--- src/helpers/webAudio.js
+++ src/helpers/webAudio.js
@@ -3,6 +3,10 @@ export const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
 export const playAudioBuffer = (audioBuffer, time = 0) => {
   const audioBufferSource = audioCtx.createBufferSource()
   audioBufferSource.buffer = audioBuffer
-  audioBufferSource.connect(audioCtx.destination)
+  const gainNode = audioCtx.createGain()
+  gainNode.gain.value = 0.6
+
+  audioBufferSource.connect(gainNode)
+  gainNode.connect(audioCtx.destination)
   audioBufferSource.start(time)
 }
